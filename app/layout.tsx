c0612cb5eff16d085bf5e9d09553c550293a0102
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2026-02-25 23:37:01 -0300

    feat: install hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { WhatsAppFloatingButton } from "@/features/chat";
 import { LanguageProvider } from "@/features/i18n";
 import { ThemeProvider } from "@/features/theme";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Gustavo Mota | AI Assistant",
@@ -23,6 +24,19 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* Hotjar Tracking Code for Site 6656104 */}
+        <Script id="hotjar-script" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6656104,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
         <ThemeProvider>
           <LanguageProvider>
             {children}
